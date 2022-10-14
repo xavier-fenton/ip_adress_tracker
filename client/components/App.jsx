378858324cee31f119e4a0155a34bdabb49e036d
xavier-fenton <xavierfenton99@gmail.com>
2022-10-14 20:32:58 +1300

    add basic html form

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 function App() {
   return (
     <div>
-      <h1>Widgets for the win!</h1>
+      <h1>hello</h1>
+      <form>
+        Insert IP:
+        <input type="text" />
+        <input type="submit" value="submit" />
+      </form>
     </div>
   )
 }
